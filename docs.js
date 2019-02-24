1975231b1be76c15048ea60587bfce726aa1c62b
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-02-24 21:59:37 +0530

    Initial commit

---
+++ docs.js
@@ -0,0 +1,27 @@
+function text1() {
+
+                var a = document.getElementById('text1');
+                a.style.display=("block");
+                var b = document.getElementById('text2');
+                b.style.display=("none");
+                var c = document.getElementById('text3');
+                c.style.display=("none");
+}
+function text2() {
+
+                var a = document.getElementById('text1');
+                a.style.display=("none");
+                var b = document.getElementById('text2');
+                b.style.display=("block");
+                var c = document.getElementById('text3');
+                c.style.display=("none");
+}
+function text3() {
+
+                var a = document.getElementById('text1');
+                a.style.display=("none");
+                var b = document.getElementById('text2');
+                b.style.display=("none");
+                var c = document.getElementById('text3');
+                c.style.display=("block");
+}
---
+++ info.js
@@ -0,0 +1,218 @@
+function mod1() {
+
+                var a = document.getElementById('mod1');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod2() {
+
+                var a = document.getElementById('mod2');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod3() {
+
+                var a = document.getElementById('mod3');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod4() {
+
+                var a = document.getElementById('mod4');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod5() {
+
+                var a = document.getElementById('mod5');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod6() {
+
+                var a = document.getElementById('mod6');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod7() {
+
+                var a = document.getElementById('mod7');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod8() {
+
+                var a = document.getElementById('mod8');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+
+function mod9() {
+
+                var a = document.getElementById('mod9');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod10() {
+
+                var a = document.getElementById('mod10');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function mod11() {
+
+                var a = document.getElementById('mod11');
+
+                a.style.opacity = ("1");
+
+                a.style.width = ("100%");
+
+            }
+            
+function cmod1() {
+
+                var a = document.getElementById('mod1');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod2() {
+
+                var a = document.getElementById('mod2');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod3() {
+
+                var a = document.getElementById('mod3');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }            
+function cmod4() {
+
+                var a = document.getElementById('mod4');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod5() {
+
+                var a = document.getElementById('mod5');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod6() {
+
+                var a = document.getElementById('mod6');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+
+function cmod7() {
+
+                var a = document.getElementById('mod7');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod8() {
+
+                var a = document.getElementById('mod8');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod9() {
+
+                var a = document.getElementById('mod9');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+
+function cmod10() {
+
+                var a = document.getElementById('mod10');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
+            
+function cmod11() {
+
+                var a = document.getElementById('mod11');
+
+                a.style.opacity = ("0");
+
+                a.style.width = ("0%");
+
+            }
